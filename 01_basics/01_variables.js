b1aa3bff68804c393607d7de4751e52497b5e23f
Rudreshkumar-1 <rudreshkumar008@gmail.com>
2025-06-05 03:18:01 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = "2372"
+let accountEmail = "bala@google.com"
+var accountPassword = "1234"
+accountCity = "Jaipur"
+
+accountEmail = "hc@hc.com"
+accountPassword = "212121"
+accountCity = "satna"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountId]);
