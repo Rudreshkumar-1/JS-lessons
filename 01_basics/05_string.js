4efa2efdb0be729b935a4fd5733e88a63ad3372c
Rudreshkumar-1 <rudreshkumar008@gmail.com>
2025-06-06 15:57:53 +0000

    more

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -5,5 +5,31 @@ const repoCount = 50
 
 console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('balaji')
-console,.
+const gameName = new String('bala-ji')
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());        
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('l'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-6, 4)
+console.group(anotherString);
+
+const newString1 = "     bala    "
+console.log(newString1);
+console.log(newString1.trim());
+
+const url = "https://bala.com/bala%20kumar"
+
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('bala'));
+console.log(url.includes('hello'));
+
+console.log(gameName.split('-'));
