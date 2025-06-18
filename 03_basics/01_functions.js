23838db2fa99654545f3d19838caecc100c070f3
Rudreshkumar-1 <rudreshkumar008@gmail.com>
2025-06-18 17:54:04 +0000

    func

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,15 @@
+function sayMyName(){
+    console.log("B");
+    console.log("A");
+    console.log("L");
+    console.log("A");
+}
+
+sayMyName()
+
+function addTwoNumbers(number1, number2){
+    console.log(number1 + number2);
+}
+addTwoNumbers(3, 4)
+addTwoNumbers(3, "4");
+addTwoNumbers(3, null);
